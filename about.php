40d48d04150e9234b00a8c45b2d7cc3472527305
MARCO TULIO SANCHEZ ESPINOSA <ing.marcotsanchez@gmail.com>
2023-11-30 18:17:25 -0500

    pagina y estilos de about

---
+++ about.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Constructores e Ingenieros</title>
+  <meta name="author" content="Marco Tulio Sánchez Espinosa">
+  <meta name="copyright" content="&copy; Constructores e Ingenieros - G & G S.A.S Derechos reservados">
+  <meta name="email" content="info@constructoreseingenieros.com">
+  <meta name="robots" content="index.follow"> <!--enlace a los navegadores-->
+  <meta name="description" content="empresa constructora de proyectos adsequibles de alta calidad comprometidos con el medio ambiente con ingenieros altamente calificados y profesionales.">
+  <meta name="keywords" content="construcción, proyectos, ingenieros, constructora, Tolima">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Constructores e Ingenieros - G & G S.A.S">
+  <meta property="og:description" content="empresa constructora de proyectos adsequibles de alta calidad comprometidos con el medio ambiente con ingenieros altamente calificados y profesionales.">
+  <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  <!-- BoxIcons -->
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <!-- Estilos propios -->
+  <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+    <header class="header-about">
+        <div class="menu container">
+        <a href="index.php" class="logo"><img src="public/img/logo.png" alt="logo de la empresa"></a>
+        <input type="checkbox" id="menu">
+        <label for="menu">
+            <img src="public/img/menu.svg" alt="menu" class="menu-icon">
+        </label>
+        <nav class="navbar">
+            <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="about.php">Nosotros</a></li>
+            <li><a href="#services">Servicios</a></li>
+            <li><a href="#projects">Proyectos</a></li>
+            <li><a href="#gallery">Galería</a></li>
+            <li><a href="#contact">Contáctenos</a></li>
+            </ul>
+        </nav>
+        </div>
+    </header>
+    <section class="about column container" id="about">
+        <div class="about-img">
+            <img src="public/img/about.jpg" alt="imagen de la empresa">
+        </div>
+        <div class="about-content">
+            <h2>Quienes Somos?</h2>
+            <p class="text-parrafo">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error numquam porro nobis eaque tenetur architecto corrupti incidunt ad repellat laborum? Ad suscipit sed vel perspiciatis esse nam, repellat eius id?</p>
+        </div>
+    </section>
+    <section class="experience row flex container">
+        <div class="experience-cards row">
+            <div class="e-card">
+                <h3>3</h3>
+                <p>Proyectos</p>
+            </div>
+            <div class="e-card">
+                <h3>240</h3>
+                <p>Construcciones</p>
+            </div>
+            <div class="e-card">
+                <h3>9</h3>
+                <p>Años de experiencia</p>
+            </div>
+            <div class="e-card">
+                <h3>80</h3>
+                <p>Remodelaciones</p>
+            </div>
+        </div>
+        <div class="experience-content">
+            <h2>9 años de experiencia</h2>
+            <p class="text-parrafo">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error numquam porro nobis eaque tenetur architecto corrupti incidunt ad repellat laborum? Ad suscipit sed vel perspiciatis esse nam, repellat eius id?</p>
+            <a href="#" class="btn btn-contact">Contáctenos</a>
+        </div>
+    </section>
+    <section class="subscribe">
+        <div class="subscribe-content container">
+            <h3>Realice una consulta con un asesor.</h3>
+            <p>Llamanos al siguiente número <strong><a href="tel:+573214614550">314 333 2345</a></strong></p>
+            <a href="#" class="btn btn-subscribe">Agende una cita!</a>
+        </div>
+    </section>
+    <footer class="footer" class="footer">
+        <div class="footer-content container">
+
+        </div>
+    </footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,6 +4,20 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Constructores e Ingenieros</title>
+  <meta name="author" content="Marco Tulio Sánchez Espinosa">
+  <meta name="copyright" content="&copy; Constructores e Ingenieros - G & G S.A.S Derechos reservados">
+  <meta name="email" content="info@constructoreseingenieros.com">
+  <meta name="robots" content="index.follow"> <!--enlace a los navegadores-->
+  <meta name="description" content="empresa constructora de proyectos adsequibles de alta calidad comprometidos con el medio ambiente con ingenieros altamente calificados y profesionales.">
+  <meta name="keywords" content="construcción, proyectos, ingenieros, constructora, Tolima">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Constructores e Ingenieros - G & G S.A.S">
+  <meta property="og:description" content="empresa constructora de proyectos adsequibles de alta calidad comprometidos con el medio ambiente con ingenieros altamente calificados y profesionales.">
+  <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  <!-- BoxIcons -->
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <!-- Estilos propios -->
   <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
@@ -16,11 +30,12 @@
       </label>
       <nav class="navbar">
         <ul>
-          <li><a href="#">Inicio</a></li>
-          <li><a href="#">Nosotros</a></li>
-          <li><a href="#">Servicios</a></li>
-          <li><a href="#">Proyectos</a></li>
-          <li><a href="#">Contactenos</a></li>
+          <li><a href="index.php">Inicio</a></li>
+          <li><a href="about.php">Nosotros</a></li>
+          <li><a href="#services">Servicios</a></li>
+          <li><a href="#projects">Proyectos</a></li>
+          <li><a href="#gallery">Galería</a></li>
+          <li><a href="#contact">Contáctenos</a></li>
         </ul>
       </nav>
     </div>
@@ -33,31 +48,68 @@
       </div>
     </div>
   </header>
-  <section id="services">
+  <section id="services" class="services">
     <div class="services-content container">
-
+      <h2 class="titles">Servicios</h2>
+    </div>
+    <div class="services-container container">
+      <div class="box">
+        <i class='bx bxl-deezer'></i>
+        <h4>servicio 1</h4>
+        <p class="text-parrafo">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error numquam porro nobis eaque tenetur architecto corrupti incidunt ad repellat laborum? Ad suscipit sed vel perspiciatis esse nam, repellat eius id?</p>
+        <a href="#" class="btn btn-contact">Saber más</a>
+      </div>
+      <div class="box">
+        <i class='bx bxl-deezer'></i>
+        <h4>servicio 1</h4>
+        <p class="text-parrafo">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error numquam porro nobis eaque tenetur architecto corrupti incidunt ad repellat laborum? Ad suscipit sed vel perspiciatis esse nam, repellat eius id?</p>
+        <a href="#" class="btn btn-contact">Saber más</a>
+      </div>
+      <div class="box">
+        <i class='bx bxl-deezer'></i>
+        <h4>servicio 1</h4>
+        <p class="text-parrafo">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error numquam porro nobis eaque tenetur architecto corrupti incidunt ad repellat laborum? Ad suscipit sed vel perspiciatis esse nam, repellat eius id?</p>
+        <a href="#" class="btn btn-contact">Saber más</a>
+      </div>
+      <div class="box">
+        <i class='bx bxl-deezer'></i>
+        <h4>servicio 1</h4>
+        <p class="text-parrafo">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error numquam porro nobis eaque tenetur architecto corrupti incidunt ad repellat laborum? Ad suscipit sed vel perspiciatis esse nam, repellat eius id?</p>
+        <a href="#" class="btn btn-contact">Saber más</a>
+      </div>
+      <div class="box">
+        <i class='bx bxl-deezer'></i>
+        <h4>servicio 1</h4>
+        <p class="text-parrafo">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error numquam porro nobis eaque tenetur architecto corrupti incidunt ad repellat laborum? Ad suscipit sed vel perspiciatis esse nam, repellat eius id?</p>
+        <a href="#" class="btn btn-contact">Saber más</a>
+      </div>
+      <div class="box">
+        <i class='bx bxl-deezer'></i>
+        <h4>servicio 1</h4>
+        <p class="text-parrafo">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error numquam porro nobis eaque tenetur architecto corrupti incidunt ad repellat laborum? Ad suscipit sed vel perspiciatis esse nam, repellat eius id?</p>
+        <a href="#" class="btn btn-contact">Saber más</a>
+      </div>
     </div>
   </section>
-  <section id="projects">
+  <section id="projects" class="projects">
     <div class="projects-content container">
-
+      <h2 class="titles">Proyectos</h2>
     </div>
   </section>
-  <section id="gallery">
+  <section id="gallery" class="gallery">
     <div class="gallery-content container">
-
+    <h2 class="titles">Galería</h2>
     </div>
   </section>
-  <section class="contact">
+  <section class="contact" class="contact">
     <div class="contact-content container">
-
+    <h2 class="titles">Contáctenos</h2>
     </div>
   </section>
-  <footer class="footer">
+  <footer class="footer" class="footer">
     <div class="footer-content container">
 
     </div>
   </footer>
-  <script src="public/js/main.js"></script>
 </body>
 </html>
